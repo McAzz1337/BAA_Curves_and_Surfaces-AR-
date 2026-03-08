17498f881e0fc2795f009d57701e5c04563affb0
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2026-03-08 18:56:58 +0100

    Made the script collect the transforms of all its children automatically

--- Assets/Scripts/ControlPoints.cs
+++ Assets/Scripts/ControlPoints.cs
@@ -1,11 +1,18 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ControlPoints : MonoBehaviour
 {
-    [SerializeField]
-    public List<Transform> controlPoints;
+    private Transform[] transforms;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Awake()
+    {
+        transforms = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transforms[i] = transform.GetChild(i);
+        }
+    }
     void Start()
     {
 
@@ -15,5 +22,10 @@ public class ControlPoints : MonoBehaviour
     void Update()
     {
 
+    }
+
+    public Transform[] getTransforms()
+    {
+        return transforms;
     }
 }
