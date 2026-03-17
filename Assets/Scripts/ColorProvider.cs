e77326b2414984a3b7f455bd80f512f1be61f7ec
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2026-03-17 20:43:35 +0100

    Added class that provides materials

---
+++ Assets/Scripts/ColorProvider.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ColorProvider : MonoBehaviour
+{
+
+    [SerializeField]
+    public Material red;
+    [SerializeField]
+    public Material green;
+    [SerializeField]
+    public Material blue;
+    [SerializeField]
+    public Material purple;
+    [SerializeField]
+    public Material yellow;
+
+}
