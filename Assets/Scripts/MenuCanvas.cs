570b8115e498f7fab74b152999bbea23014de2fa
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2026-03-27 19:32:57 +0100

    Complete rework

--- Assets/Scripts/MenuCanvas.cs
+++ Assets/Scripts/MenuCanvas.cs
@@ -1,17 +1,78 @@
 using UnityEngine;
+using Oculus.Interaction.Input;
+using Meta.XR.ImmersiveDebugger;
 
 public class MenuCanvas : MonoBehaviour
 {
 
+    public Hand leftHand;
+    public Hand rightHand;
 
-    public Transform cam;
-    public Vector3 offset = new Vector3(-0.3f, 0.2f, 0.6f);
+    [DebugMember]
+    public bool leftPinching;
+    [DebugMember]
+    public bool rightPinching;
 
-    void LateUpdate()
+    [DebugMember]
+    public bool active = false;
+
+    private bool leftWasPinchingLastFrame;
+    private bool rightWasPinchingLastFrame;
+
+    public CanvasGroup canvasGroup;
+
+    public Camera cam;
+    public float offsetX = 0.1f;
+    public float offsetZ = 0.3f;
+
+    void Aake()
     {
-        transform.position = cam.position + cam.TransformDirection(offset);
-        transform.rotation = Quaternion.LookRotation(
-            transform.position - cam.position
-        );
+        toggleCanvas(active);
+    }
+
+    void Update()
+    {
+        bool leftPinching = detectPinch(leftHand);
+        bool rightPinching = detectPinch(rightHand);
+
+        if ((leftPinching && !leftWasPinchingLastFrame) ||
+         (rightPinching && !rightWasPinchingLastFrame))
+        {
+            active = !active;
+            toggleCanvas(active);
+            if (active)
+            {
+                positioncanvas();
+            }
+        }
+
+        leftWasPinchingLastFrame = leftPinching;
+        rightWasPinchingLastFrame = rightPinching;
+    }
+
+    private void toggleCanvas(bool show)
+    {
+        canvasGroup.alpha = show ? 1 : 0;
+        canvasGroup.interactable = show;
+        canvasGroup.blocksRaycasts = show;
+    }
+
+    private void positioncanvas()
+    {
+        transform.position = cam.transform.position + new Vector3(0.0f, 0.0f, offsetZ);
+
+        Quaternion rot = Quaternion.LookRotation(
+            transform.position - cam.transform.position);
+
+        transform.rotation = rot;
+        transform.position += new Vector3(offsetX, 0.0f, 0.0f);
+    }
+
+    private bool detectPinch(Hand hand)
+    {
+        bool thumbPinching = hand.GetFingerIsPinching(HandFinger.Thumb);
+        bool ringPinching = hand.GetFingerIsPinching(HandFinger.Ring);
+
+        return thumbPinching && ringPinching;
     }
 }
