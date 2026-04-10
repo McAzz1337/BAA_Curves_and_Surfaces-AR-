3b1a1d1450ea9434dd803680f23cf4afa8487d3f
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2026-04-10 20:40:45 +0200

    Removed references to control point pool

--- Assets/Scripts/ControlPoints.cs
+++ Assets/Scripts/ControlPoints.cs
@@ -1,21 +1,9 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ControlPoints : MonoBehaviour
 {
     private Transform[] transforms;
-    [SerializeField]
-    private ControlPointsPool controlPointPool;
-    public ControlPointsPool ControlPointPool
-    {
-        set { controlPointPool = value; }
-    }
 
-    public bool log = false;
-    void Awake()
-    {
-
-    }
     void Start()
     {
         gatherControlPoints();
@@ -29,41 +17,16 @@ public class ControlPoints : MonoBehaviour
 
     public Transform[] getTransforms()
     {
-        if (log)
-        {
-            Debug.Log("Getting points: " + transforms.Length);
-            log = false;
-        }
         return transforms;
-    }
-
-    public void addControlPoint(Transform transform)
-    {
-        Debug.Log("ControlPoint added: " + transforms.Length);
     }
 
     public void gatherControlPoints()
     {
         int count = transform.childCount;
-        Debug.Log("Gather: " + count);
         transforms = new Transform[count];
         for (int i = 0; i < count; i++)
         {
             transforms[i] = transform.GetChild(i);
         }
     }
-
-
-    void OnDestroy()
-    {
-        if (controlPointPool != null)
-        {
-            foreach (var cp in transforms)
-            {
-                controlPointPool.insertControlPoint(cp.gameObject);
-            }
-        }
-    }
-
-
 }
