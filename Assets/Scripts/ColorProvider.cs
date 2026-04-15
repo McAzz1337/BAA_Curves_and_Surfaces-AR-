e6bcf91670146d4ce0ba8864c711a0f4a773dbe3
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2026-04-15 18:22:03 +0200

    Added Colors

--- Assets/Scripts/ColorProvider.cs
+++ Assets/Scripts/ColorProvider.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class ColorProvider : MonoBehaviour
 {
+    [SerializeField]
+    public Material white;
 
     [SerializeField]
     public Material red;
     [SerializeField]
     public Material green;
+    [SerializeField]
+    public Material cyan;
     [SerializeField]
     public Material blue;
     [SerializeField]
